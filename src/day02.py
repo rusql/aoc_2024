3a7b228ab5240d524166f606e8a4a2876ad3be02
Russell Smith <russell.subscriptions@gmail.com>
2024-12-04 16:05:35 +0200

    add day02 part1

--- src/day02.py
+++ src/day02.py
@@ -1,2 +1,55 @@
 # solving: https://adventofcode.com/2024/day/1
 
+
+from pathlib import Path
+
+
+def get_data(path_to_file):
+    reports = []
+    with open(path_to_file) as f:
+        for line in f:
+            items = line.split(" ")
+            int_items = list(map(lambda x: int(x), items))
+            reports.append(int_items)
+    return reports
+
+
+def level_safe(level: list[int]) -> bool:
+    # edge case: single item
+    if len(level) <= 1:
+        return True
+
+    # first items equal
+    if level[0] == level[1]:
+        return False
+
+    # determine trend (increasing or decreasing)
+    is_increasing = level[0] - level[1] > 0
+
+    prev_item = None
+    for item in level:
+        if prev_item == None:
+            prev_item = item
+            continue
+        diff = abs(item - prev_item)
+        if diff < 1 or diff > 3:
+            return False
+        if is_increasing and (prev_item <= item):
+            return False
+        if not is_increasing and (prev_item >= item):
+            return False
+        prev_item = item
+    return True
+
+
+def safe_level_count(levels):
+    safe_levels = 0
+    for level in levels:
+        if level_safe(level):
+            safe_levels += 1
+
+    return safe_levels
+
+
+input = get_data(f"{Path(__file__).resolve().parents[1]}/input/day02.txt")
+print (f"Safe levels: {safe_level_count(input)}")
--- src/test_day02.py
+++ src/test_day02.py
@@ -1,5 +1,12 @@
 import unittest
+from day02 import level_safe
+
 
 class TestDay02(unittest.TestCase):
-    def test_stub(self):
-        self.assertEqual(1,21)
+    def test_level_safe(self):
+        self.assertEqual(level_safe([7, 6, 4, 2, 1]), True)
+        self.assertEqual(level_safe([1, 2, 7, 8, 9]), False)
+        self.assertEqual(level_safe([9, 7, 6, 2, 1]), False)
+        self.assertEqual(level_safe([1, 3, 2, 4, 5]), False)
+        self.assertEqual(level_safe([8, 6, 4, 4, 1]), False)
+        self.assertEqual(level_safe([1, 3, 6, 7, 9]), True)
